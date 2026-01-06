bb845f110f4e6a5f591d30b71ab0a06340feb2a6
doorcs <itsdoorcs@gmail.com>
2026-01-06 23:56:56 +0900

    Lv.1 여러 기준으로 정렬하기

---
+++ SQL/Programmers/여러_기준으로_정렬하기.sql
@@ -0,0 +1,12 @@
+SELECT
+    ANIMAL_ID,
+    NAME,
+    DATEIMTE
+FROM
+    ANIMAL_INS
+ORDER BY
+    NAME ASC,
+    DATETIME DESC;
+
+-- 기본적으로 키워드는 대문자, 테이블명과 칼럼명은 소문자로 쓰려고 하되,
+-- 지금처럼 문제 조건에서 테이블명, 칼럼명을 대문자로 주는 경우에는 그대로 쓰자
