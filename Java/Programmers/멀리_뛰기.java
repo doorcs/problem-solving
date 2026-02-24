6f1a25042604d3fa123a1ddf9e88778659a4ab4c
doorcs <itsdoorcs@gmail.com>
2026-02-24 22:02:25 +0900

    Lv.2 멀리 뛰기
    DP

---
+++ Java/Programmers/멀리_뛰기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+
+    int[] dp = new int[2001];
+
+    public long solution(int n) {
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i-1] + dp[i-2]) % 1_234_567;
+        }
+
+        return dp[n];
+    }
+}
