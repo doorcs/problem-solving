a93e327ada295d4c0415d90ee48547c18859cb94
doorcs <itsdoorcs@gmail.com>
2026-03-29 11:19:56 +0900

    11053번: 가장 긴 증가하는 부분 수열
    DP

---
+++ Java/BOJ/11053/Main.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int N;
+    static int[] arr = new int[1001];
+    static int[] dp = new int[1001];
+
+    public static void main(String[] args) {
+        Arrays.fill(dp, 1);
+        N = Integer.parseInt(readLine());
+        var st = new StringTokenizer(readLine());
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int longest = 1;
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j]+1);
+                    longest = Math.max(longest, dp[i]);
+                }
+            }
+        }
+
+        System.out.println(longest);
+    }
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    static String readLine() {
+        try {
+            return br.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
